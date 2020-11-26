c49df8cc87e5e88a0dd604d4a2ab265c057b492e
TomJSoftwire <thomas.johnston@softwire.com>
2020-11-26 14:46:51 +0000

    JDBI

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -5,18 +5,19 @@ import org.jdbi.v3.core.Jdbi;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        String hostname = "localhost";
-        String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
-        String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
+        String hostname = "localhost:3306";
+        String database = "library";
+        String user = "root";
+        String password = "admin";
+        String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password+ "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
-        jdbcMethod(connectionString);
+        //jdbcMethod(connectionString);
         jdbiMethod(connectionString);
     }
 
@@ -40,7 +41,12 @@ public class Main {
         // Use the "Book" class that we've created for you (in the models.database folder)
 
         Jdbi jdbi = Jdbi.create(connectionString);
-
+        List<String> test = jdbi.withHandle(handle -> {
+                return handle.createQuery("SELECT title FROM library.book")
+                        .mapTo(String.class)
+                        .list();
+        });
+        System.out.println(test);
 
 
     }
